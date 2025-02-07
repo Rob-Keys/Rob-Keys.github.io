97626bd1b95af7716bf4f0d41cbe81e60f4148fb
Rob <157063279+Rob-Keys@users.noreply.github.com>
2025-02-07 12:07:48 -0500

    Added persistence of theme across pages

--- js/themes.js
+++ js/themes.js
@@ -1,9 +1,11 @@
-function setTheme(themeName) {
-    const root = document.documentElement;
-    let theme = themeName;
-    root.style.setProperty('--active-main', "var(--" + theme + "-main)");
-    root.style.setProperty('--active-secondary', "var(--" + theme + "-secondary)");
-    root.style.setProperty('--active-accent1', "var(--" + theme + "-accent1)");
-    root.style.setProperty('--active-accent2', "var(--" + theme + "-accent2)");
-    root.style.setProperty('--active-accent3', "var(--" + theme + "-accent3)");
+function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme); // Set theme attribute
+    localStorage.setItem('theme', theme); // Store theme in localStorage
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+    }
+});
